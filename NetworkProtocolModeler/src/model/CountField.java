72b04adeaa7b8c058c35b7464c8ea4b48eff3f79
Gábor Kövesdán <gabor@kovesdan.org>
2014-04-05 17:59:56 +0200

    - Add forgotten model classes

---
+++ NetworkProtocolModeler/src/model/CountField.java
@@ -0,0 +1,158 @@
+/**
+ */
+package model;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Count Field</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link model.CountField#getRef <em>Ref</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see model.ProtocolPackage#getCountField()
+ * @model kind="class"
+ * @generated
+ */
+public class CountField extends Field {
+	/**
+	 * The cached value of the '{@link #getRef() <em>Ref</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getRef()
+	 * @generated
+	 * @ordered
+	 */
+	protected ListField ref;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected CountField() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ProtocolPackage.Literals.COUNT_FIELD;
+	}
+
+	/**
+	 * Returns the value of the '<em><b>Ref</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Ref</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Ref</em>' reference.
+	 * @see #setRef(ListField)
+	 * @see model.ProtocolPackage#getCountField_Ref()
+	 * @model required="true"
+	 * @generated
+	 */
+	public ListField getRef() {
+		if (ref != null && ref.eIsProxy()) {
+			InternalEObject oldRef = (InternalEObject)ref;
+			ref = (ListField)eResolveProxy(oldRef);
+			if (ref != oldRef) {
+			}
+		}
+		return ref;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ListField basicGetRef() {
+		return ref;
+	}
+
+	/**
+	 * Sets the value of the '{@link model.CountField#getRef <em>Ref</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Ref</em>' reference.
+	 * @see #getRef()
+	 * @generated
+	 */
+	public void setRef(ListField newRef) {
+		ref = newRef;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case ProtocolPackage.COUNT_FIELD__REF:
+				if (resolve) return getRef();
+				return basicGetRef();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case ProtocolPackage.COUNT_FIELD__REF:
+				setRef((ListField)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case ProtocolPackage.COUNT_FIELD__REF:
+				setRef((ListField)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case ProtocolPackage.COUNT_FIELD__REF:
+				return ref != null;
+		}
+		return super.eIsSet(featureID);
+	}
+
+} // CountField
---
+++ NetworkProtocolModeler/src/model/ListField.java
@@ -0,0 +1,158 @@
+/**
+ */
+package model;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>List Field</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link model.ListField#getElementType <em>Element Type</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see model.ProtocolPackage#getListField()
+ * @model kind="class"
+ * @generated
+ */
+public class ListField extends Field {
+	/**
+	 * The cached value of the '{@link #getElementType() <em>Element Type</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getElementType()
+	 * @generated
+	 * @ordered
+	 */
+	protected Field elementType;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ListField() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ProtocolPackage.Literals.LIST_FIELD;
+	}
+
+	/**
+	 * Returns the value of the '<em><b>Element Type</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Element Type</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Element Type</em>' reference.
+	 * @see #setElementType(Field)
+	 * @see model.ProtocolPackage#getListField_ElementType()
+	 * @model required="true"
+	 * @generated
+	 */
+	public Field getElementType() {
+		if (elementType != null && elementType.eIsProxy()) {
+			InternalEObject oldElementType = (InternalEObject)elementType;
+			elementType = (Field)eResolveProxy(oldElementType);
+			if (elementType != oldElementType) {
+			}
+		}
+		return elementType;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Field basicGetElementType() {
+		return elementType;
+	}
+
+	/**
+	 * Sets the value of the '{@link model.ListField#getElementType <em>Element Type</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Element Type</em>' reference.
+	 * @see #getElementType()
+	 * @generated
+	 */
+	public void setElementType(Field newElementType) {
+		elementType = newElementType;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case ProtocolPackage.LIST_FIELD__ELEMENT_TYPE:
+				if (resolve) return getElementType();
+				return basicGetElementType();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case ProtocolPackage.LIST_FIELD__ELEMENT_TYPE:
+				setElementType((Field)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case ProtocolPackage.LIST_FIELD__ELEMENT_TYPE:
+				setElementType((Field)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case ProtocolPackage.LIST_FIELD__ELEMENT_TYPE:
+				return elementType != null;
+		}
+		return super.eIsSet(featureID);
+	}
+
+} // ListField
