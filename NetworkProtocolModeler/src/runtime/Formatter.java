7cddc316733766ccb64352b55f50af511279314f
Gábor Kövesdán <gabor@kovesdan.org>
2014-04-15 10:03:43 +0200

    - Formatters must work with ByteBuffer not byte array

--- NetworkProtocolModeler/src/runtime/Formatter.java
+++ NetworkProtocolModeler/src/runtime/Formatter.java
@@ -1,6 +1,9 @@
 package runtime;
 
+import java.nio.ByteBuffer;
+
 public interface Formatter<T> {
 	byte[] toBytes(T t);
-	int parseFromBytes(byte[] bytes, int off, T value);
+
+	int parseFromBytes(ByteBuffer bytes, int off, T value);
 }
