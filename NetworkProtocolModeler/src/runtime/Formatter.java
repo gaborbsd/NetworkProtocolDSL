177109ea7f19b6a6d9f597c3681de0adf8886775
Gábor Kövesdán <gabor@kovesdan.org>
2014-04-15 10:27:45 +0200

    - Readjust more the Formatter interface

--- NetworkProtocolModeler/src/runtime/Formatter.java
+++ NetworkProtocolModeler/src/runtime/Formatter.java
@@ -5,5 +5,5 @@ import java.nio.ByteBuffer;
 public interface Formatter<T> {
 	byte[] toBytes(T t);
 
-	int parseFromBytes(ByteBuffer bytes, int off, T value);
+	T parseFromBytes(ByteBuffer buffer);
 }
